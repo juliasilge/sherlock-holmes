48492d4ff7cb238be601bcec9b4fd7c921d9338b
Julia Silge <julia.silge@gmail.com>
2017-10-27 18:01:04 -0600

    Transition to deploy app with Docker

--- app-files/run.R
+++ app-files/run.R
@@ -1,8 +1,8 @@
 library(shiny)  
 port <- Sys.getenv('PORT')
 
-shiny::runApp(  
-    appDir = getwd(),
+rmarkdown::run(  
+    '/home/rstudio/sherlock-topic-model-app.Rmd',
     host = '0.0.0.0',
     port = as.numeric(port)
 )
